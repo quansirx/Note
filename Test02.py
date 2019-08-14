1af4d9edb9b1582e1ad45213e968e41cd2877afa
qjm <qjm>
2019-08-14 17:39:05 +0800

    Python测试02

---
+++ Test02.py
@@ -0,0 +1,73 @@
+
+#!/usr/bin/python3
+ 
+'''class MyClass:
+    """一个简单的类实例"""
+    i = 12345
+    def f(self):
+        return 'hello world'
+ 
+# 实例化类
+x = MyClass()
+ 
+# 访问类的属性和方法
+print("MyClass 类的属性 i 为：", x.i)
+print("MyClass 类的方法 f 输出为：", x.f())
+print(MyClass.i)
+print(MyClass.f(1))
+
+
+class people:
+    #定义基本属性
+    name = ''
+    age = 0
+    #定义私有属性,私有属性在类外部无法直接进行访问
+    __weight = 0
+    #定义构造方法
+    def __init__(self,n,a,w):
+        self.name = n
+        self.age = a
+        self.__weight = w
+    def speak(self):
+        print("%s 说: 我 %d 岁。" %(self.name,self.age))
+ 
+# 实例化类
+p = people('runoob',10,30)
+p.speak()
+print(people.age)
+print(p.age)
+
+
+class Test:
+    def prt(runoob):
+        print(runoob)
+        print(runoob.__class__)
+ 
+t = Test()
+t.prt()
+Test().prt()'''
+
+class Vector:
+   def __init__(self, a, b):
+      self.a = a
+      self.b = b
+ 
+   def __str__(self):
+      return 'Vector (%d, %d)' % (self.a, self.b)
+   
+   def __add__(self,other):
+      return Vector(self.a + other.a, self.b + other.b)
+ 
+v1 = Vector(2,10)
+v2 = Vector(5,-2)
+print (v1 + v2)
+
+
+import calendar
+ 
+# 输入指定年月
+yy = int(input("输入年份: "))
+mm = int(input("输入月份: "))
+ 
+# 显示日历
+print(calendar.month(yy,mm))
